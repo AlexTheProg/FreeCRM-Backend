1d2e74956935aa681efa4dcad615ecdb55ce57e3
AlexTheProg <alexandru.contu99@gmail.com>
2022-12-20 02:23:25 +0200

    refactor commit

---
+++ src/main/java/com/example/freecrmbackend/FreeCrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.freecrmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeCrmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeCrmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/freecrmbackend/FreeCrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freecrmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeCrmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
